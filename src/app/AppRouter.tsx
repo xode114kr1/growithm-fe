fa8183374aa3579f69545fcc7049be0de4d58771
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-11-29 22:25:45 +0900

    feat : init HomePage

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,7 +1,12 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
 
 const AppRouter = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
