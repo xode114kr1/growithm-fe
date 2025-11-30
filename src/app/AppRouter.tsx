1e3f214378e2fd839cb84fb73d565c46ae55f0e0
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-11-30 20:59:38 +0900

    feat : CallbackPage 라우팅 설정

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Outlet, Route, Routes } from "react-router-dom";
 import Header from "../shared/components/Header";
 import HomePage from "../pages/HomePage/HomePage";
 import Footer from "../shared/components/Footer";
+import CallbackPage from "../pages/CallbackPage/CallbackPage";
 
 const Layout = () => {
   return (
@@ -16,6 +17,7 @@ const Layout = () => {
 const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/callback" element={<CallbackPage />} />
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
       </Route>
