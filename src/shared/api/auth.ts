0c83d92e5637872e2ee9641ba9c2ac50747655b4
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-11-30 22:06:08 +0900

    fix : login url 수정

--- src/shared/api/auth.ts
+++ src/shared/api/auth.ts
@@ -6,7 +6,7 @@ const GITHUB_CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENT_ID;
 const GITHUB_REDIRECT_URI = import.meta.env.VITE_GITHUB_REDIRECT_URI;
 const GITHUB_SCOPE = import.meta.env.VITE_GITHUB_SCOPE;
 
-const cookies = new Cookies();
+// const cookies = new Cookies();
 
 export const GITHUB_AUTH_URL =
   `https://github.com/login/oauth/authorize` +
@@ -16,12 +16,15 @@ export const GITHUB_AUTH_URL =
 
 export async function login(code: string) {
   if (!code) throw new Error("인가 코드가 없습니다.");
-  console.log(code);
   // Todo : 백엔드 주소 수정
-  const res = await axios.post(`${API_URL}/auth/github/callback?code=${code}`, {
-    credentials: "include",
-  });
-  cookies.set("accessToken", res.data.access_token, { path: "/" });
-  cookies.set("refreshToken", res.data.refresh_token, { path: "/" });
-  return res.data();
+  const res = await axios.post(
+    `${API_URL}/auth/github/callback`,
+    { code },
+    { withCredentials: true }
+  );
+
+  // Todo : 토큰 오면 처리
+  //   cookies.set("accessToken", res.data.access_token, { path: "/" });
+  //   cookies.set("refreshToken", res.data.refresh_token, { path: "/" });
+  return res.data;
 }
