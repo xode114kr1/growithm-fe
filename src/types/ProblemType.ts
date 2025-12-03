d304e1bf7b01337b2284a83c65ec5bc0b631e09e
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-12-04 04:37:59 +0900

    feat : GetPendingListProps 타입 정의

---
+++ src/types/ProblemType.ts
@@ -0,0 +1,12 @@
+export interface GetPendingListProps {
+  userId: string;
+  tier: string;
+  title: string;
+  problemId: string;
+  link: string;
+  memory: string;
+  time: string;
+  description: string;
+  code: string;
+  createdAt: string;
+}
